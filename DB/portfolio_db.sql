59094893d3a7dfee3a61af64787019097779c034
BluffDragon8211 <lortega@edenschool.fr>
2024-04-23 21:35:44 +0200

    Importing database

---
+++ DB/portfolio_db.sql
@@ -0,0 +1,40 @@
+DROP TABLE IF EXISTS `portfolio_Techno`;
+
+DROP TABLE IF EXISTS `portfolio_Image`;
+
+DROP TABLE IF EXISTS `portfolio_Project`;
+
+DROP TABLE IF EXISTS `portfolio_Admin`;
+
+CREATE TABLE IF NOT EXISTS `portfolio_Admin` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`username` varchar(50) NOT NULL,
+	`password` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `portfolio_Project` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`project_name` varchar(255) NOT NULL,
+	`project_description` text NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `portfolio_Techno` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`techno_name` varchar(255) NOT NULL,
+	`project_id` int NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `portfolio_Image` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`image_name` varchar(255) NOT NULL,
+	`project_id` int NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+
+
+ALTER TABLE `portfolio_Techno` ADD CONSTRAINT `Techno_fk2` FOREIGN KEY (`project_id`) REFERENCES `portfolio_Project`(`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `portfolio_Image` ADD CONSTRAINT `Image_fk2` FOREIGN KEY (`project_id`) REFERENCES `portfolio_Project`(`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
