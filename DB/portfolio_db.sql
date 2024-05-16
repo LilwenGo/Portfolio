b00fbcc3e0a8e70946408592fb2bb045b71106b4
BluffDragon8211 <lortega@edenschool.fr>
2024-05-16 11:15:26 +0200

    Changing Database tables

--- DB/portfolio_db.sql
+++ DB/portfolio_db.sql
@@ -1,33 +1,43 @@
-DROP TABLE IF EXISTS `portfolio_Techno`;
+START TRANSACTION;
 
-DROP TABLE IF EXISTS `portfolio_Image`;
+DROP TABLE IF EXISTS `portfolio_project_techno`;
 
-DROP TABLE IF EXISTS `portfolio_Project`;
+DROP TABLE IF EXISTS `portfolio_techno`;
 
-DROP TABLE IF EXISTS `portfolio_Admin`;
+DROP TABLE IF EXISTS `portfolio_image`;
 
-CREATE TABLE IF NOT EXISTS `portfolio_Admin` (
+DROP TABLE IF EXISTS `portfolio_project`;
+
+DROP TABLE IF EXISTS `portfolio_admin`;
+
+CREATE TABLE IF NOT EXISTS `portfolio_admin` (
 	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
 	`username` varchar(50) NOT NULL,
 	`password` varchar(255) NOT NULL,
 	PRIMARY KEY (`id`)
 )ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `portfolio_Project` (
+CREATE TABLE IF NOT EXISTS `portfolio_project` (
 	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
 	`project_name` varchar(255) NOT NULL,
 	`project_description` text NOT NULL,
 	PRIMARY KEY (`id`)
 )ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `portfolio_Techno` (
+CREATE TABLE IF NOT EXISTS `portfolio_techno` (
 	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
 	`techno_name` varchar(255) NOT NULL,
 	`project_id` int NOT NULL,
 	PRIMARY KEY (`id`)
 )ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `portfolio_Image` (
+CREATE TABLE IF NOT EXISTS `portfolio_project_techno` (
+	`project_id` int NOT NULL,
+	`techno_id` int NOT NULL,
+	PRIMARY KEY (`project_id`, `techno_id`)
+)ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `portfolio_image` (
 	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
 	`image_name` varchar(255) NOT NULL,
 	`project_id` int NOT NULL,
@@ -36,5 +46,8 @@ CREATE TABLE IF NOT EXISTS `portfolio_Image` (
 
 
 
-ALTER TABLE `portfolio_Techno` ADD CONSTRAINT `Techno_fk2` FOREIGN KEY (`project_id`) REFERENCES `portfolio_Project`(`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
-ALTER TABLE `portfolio_Image` ADD CONSTRAINT `Image_fk2` FOREIGN KEY (`project_id`) REFERENCES `portfolio_Project`(`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `portfolio_project_techno` ADD CONSTRAINT `Techno_fk1` FOREIGN KEY (`project_id`) REFERENCES `portfolio_project`(`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `portfolio_project_techno` ADD CONSTRAINT `Techno_fk2` FOREIGN KEY (`project_id`) REFERENCES `portfolio_techno`(`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `portfolio_image` ADD CONSTRAINT `Image_fk2` FOREIGN KEY (`project_id`) REFERENCES `portfolio_project`(`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
+
+COMMIT;
---
+++ DB/seeder.sql
@@ -0,0 +1,9 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+INSERT INTO `portfolio_admin` (`id`, `username`, `password`) VALUES
+(1, 'Happy', '$2y$10$sJ3xoCEiL1QRCXYUpC.mB.I90eIV1VeOJmuvtI0Ey6tkaFChbZoSG'),
+(7, 'LilwenGo', '$2y$10$sdiw.uRnzzYmHuvoFBcbS.TFn/CzkQ1EfEM8EnWaYAvaq.Qxv13Tq');
+
+COMMIT;
